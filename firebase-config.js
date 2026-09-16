7867d6c98280e4ba7020a72a6d841b20f407bbce
vvynasvane-ops <vvynasvane@gmail.com>
2026-09-16 05:18:26 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyD48AHi-3L6S96sIsJu-h7xJ61PVO5-6zs",
-    authDomain: "veusbid.firebaseapp.com",
-    projectId: "veusbid",
-    storageBucket: "veusbid.firebasestorage.app",
-    messagingSenderId: "420976430868",
-    appId: "1:420976430868:web:9224f3431ffe77523a7c01",
-    measurementId: "G-6SSXGTT2X0"
-  };
-
- 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD48AHi-3L6S96sIsJu-h7xJ61PVO5-6zs",
+  authDomain: "veusbid.firebaseapp.com",
+  projectId: "veusbid",
+  storageBucket: "veusbid.firebasestorage.app",
+  messagingSenderId: "420976430868",
+  appId: "1:420976430868:web:9224f3431ffe77523a7c01",
+  measurementId: "G-6SSXGTT2X0"
+};
